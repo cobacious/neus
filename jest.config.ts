3e949474e050771ccd01d57b4545b144e0b36940
Jacob W <thisisjacob@gmail.com>
2025-06-13 12:02:49 +0100

    chore: config tidy up

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+// jest.config.ts
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: './tsconfig.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+  roots: ['<rootDir>/apps', '<rootDir>/packages'],
+};
+
+export default config;
