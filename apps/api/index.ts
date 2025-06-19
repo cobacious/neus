036b6cd2c374497faa2aec76f699c93552691dda
Jacob W <thisisjacob@gmail.com>
2025-06-19 12:12:58 +0100

    feat(api): load env vars from dotenv (#16)

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,11 +1,16 @@
 import { createServer } from 'node:http';
 import { createYoga } from 'graphql-yoga';
 import { schema } from './schema';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const yoga = createYoga({ schema });
 
 const server = createServer(yoga);
 
-server.listen(4000, () => {
-  console.log('GraphQL server running at http://localhost:4000/graphql');
+const port = parseInt(process.env.PORT || '4000', 10);
+
+server.listen(port, () => {
+  console.log(`GraphQL server running at http://localhost:${port}/graphql`);
 });
