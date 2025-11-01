8347cfd72cb0e2f8f92112bda68984c780cbf6d6
Jacob Walton <jacob.walton@dnata.com>
2025-11-01 13:44:28 +0000

    feat(ui): add logo

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ClusterList from './components/ClusterList';
 import ClusterDetailPage from './ClusterDetailPage';
 import BetaBanner from './components/BetaBanner';
+import Logo from './components/Logo';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="max-w-3xl mx-auto p-4">
+      <div className="max-w-3xl mx-auto">
+        <header className="my-6">
+          <a href="/">
+            <Logo className="h-8 lg:h-12 w-auto mx-auto" />
+          </a>
+        </header>
         <BetaBanner />
-        <h1 className="text-2xl font-bold mb-4">Neus Clusters</h1>
         <Routes>
           <Route path="/" element={<ClusterList />} />
           <Route path="/clusters/:id" element={<ClusterDetailPage />} />
